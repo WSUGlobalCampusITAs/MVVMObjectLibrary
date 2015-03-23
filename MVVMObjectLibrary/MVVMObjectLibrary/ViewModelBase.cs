b3f66a88ef34b8bfc7ecb90c90a4104fdccfa285
davidlystad <david.lystad@email.wsu.edu>
2015-03-23 15:08:25 -0700

    Created a base library for generic MVVM applications

---
+++ MVVMObjectLibrary/MVVMObjectLibrary/ViewModelBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MVVMObjectLibrary
+{
+    /// <summary>
+    /// Base class for all project View Models
+    /// Provides interface elements for INotify, and IDisposable
+    /// </summary>
+    public class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected ViewModelBase()
+        {
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+
+        public void Dispose()
+        {
+            this.OnDispose();
+        }
+
+        protected virtual void OnDispose()
+        { }
+    }
+}
